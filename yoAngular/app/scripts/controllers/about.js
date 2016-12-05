a7412dbe7e034575a4368cde91ed25ce54c9de15
yangzh <909901649@qq.com>
2016-12-05 11:13:53 +0800

    skeleton

---
+++ yoAngular/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name demobyYzhApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the demobyYzhApp
+ */
+angular.module('demobyYzhApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ yoAngular/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name demobyYzhApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the demobyYzhApp
+ */
+angular.module('demobyYzhApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
